428c970fe9c6f838775f9756df1ba5c2b5c16739
tilmitt11191 <tilmitt11@gmail.com>
2018-11-01 17:26:15 +0900

    ubuntusetuptest 2018年 11月  1日 木曜日 17:26:15 JST

--- lib/create_symbolic_link.sh
+++ lib/create_symbolic_link.sh
@@ -5,10 +5,11 @@ INITIALDIR=`sudo pwd`
 cd `dirname $0`
 
 if [ $# != 1 ]; then echo "####lack of argument($#)"; exit 1; fi
-
+DATETIME=`date +%Y%m%d%H%M`
 DOTFILES=(vimrc vim tmux.conf fonts)
 for file in ${DOTFILES[@]}; do
-	[ $1 == "backup" ] && cp -r ~/.$file ~/."$file".`date +%Y%m%d%H%M%S`
+	#[ $1 == "backup" ] && cp -r ~/.$file ~/."$file".`date +%Y%m%d%H%M%S`
+	[ $1 == "backup" ] && [ -e ~/.$file ] && cp -r ~/.$file ~/."$file".$DATETIME
 	[ -e ~/.$file ] && rm -rf ~/.$file
 	ln -s ~/.dotfiles/$file ~/.$file
 done
