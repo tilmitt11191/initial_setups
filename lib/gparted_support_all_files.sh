ed0382d0a93e75b808b6192ca8b60daf6a4085a0
tilmitt11191 <tilmitt11@gmail.com>
2018-11-04 14:09:28 +0900

    PC 2018年 11月  4日 日曜日 14:09:28 JST

---
+++ lib/gparted_support_all_files.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo "####`basename $0` start."
+INITIALDIR=`sudo pwd`
+cd `dirname $0`
+
+
+#sudo apt -y update && sudo apt -y dist-upgrade
+
+echo "####install packages"
+PACKAGES=(btrfs-progs btrfs-tools e2fsprogs f2fs-tools hfsutils hfsprogs jfsutils util-linux cryptsetup dmsetup lvm2 nilfs-tools reiser4progs reiserfsprogs libguestfs-reiserfs udftools xfsprogs xfsdump)
+#nilfs-utils
+#reiserfs-utils
+
+for package in ${PACKAGES[@]}; do
+	dpkg -l $package | grep -E "^i.+[ \t]+$package" > /dev/null
+	if [ $? -ne 0 ];then
+		m="$package not installed. sudo apt install -y $package."
+		echo "$m"
+		sudo apt install -y $package
+	else
+		m="$package already installed."
+		echo "$m"
+	fi
+done
+
+
+
+
+cd $INITIALDIR
+exit 0
+
+: <<'#__CO__'
+#__CO__
