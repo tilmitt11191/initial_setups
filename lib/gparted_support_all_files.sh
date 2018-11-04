1052e0f51ff96944a3d9c555579abb8d968b47e9
tilmitt11191 <tilmitt11@gmail.com>
2018-11-04 14:23:38 +0900

    PC 2018年 11月  4日 日曜日 14:23:38 JST

--- lib/gparted_support_all_files.sh
+++ lib/gparted_support_all_files.sh
@@ -5,10 +5,11 @@ INITIALDIR=`sudo pwd`
 cd `dirname $0`
 
 
-#sudo apt -y update && sudo apt -y dist-upgrade
+sudo apt -y update && sudo apt -y dist-upgrade
 
 echo "####install packages"
-PACKAGES=(btrfs-progs btrfs-tools e2fsprogs f2fs-tools hfsutils hfsprogs jfsutils util-linux cryptsetup dmsetup lvm2 nilfs-tools reiser4progs reiserfsprogs libguestfs-reiserfs udftools xfsprogs xfsdump)
+PACKAGES=(btrfs-progs btrfs-tools e2fsprogs f2fs-tools hfsutils hfsprogs jfsutils util-linux cryptsetup dmsetup lvm2 nilfs-tools reiser4progs reiserfsprogs libguestfs-reiserfs udftools xfsprogs xfsdump quota
+)
 #nilfs-utils
 #reiserfs-utils
 
