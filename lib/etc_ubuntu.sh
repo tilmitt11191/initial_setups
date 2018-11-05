b65412e45df1c6aa262998117a4020ad7992f1a0
tilmitt11191 <tilmitt11@gmail.com>
2018-11-05 18:56:29 +0900

    backuptower 2018年 11月  5日 月曜日 18:56:29 JST

--- lib/etc_ubuntu.sh
+++ lib/etc_ubuntu.sh
@@ -5,6 +5,36 @@ INITIALDIR=`sudo pwd`
 cd `dirname $0`
 
 
+## install dropbox to ubuntu server 18.04
+: <<'#__CO__'
+sudo wget -O /usr/local/bin/dropbox "https://www.dropbox.com/download?dl=packages/dropbox.py"
+sudo chmod +x /usr/local/bin/dropbox
+dropbox
+echo "[Unit]
+Description=Dropbox Service
+After=network.target
+
+[Service]
+ExecStart=/bin/sh -c '/usr/local/bin/dropbox start'
+ExecStop=/bin/sh -c '/usr/local/bin/dropbox stop'
+PIDFile=${HOME}/.dropbox/dropbox.pid
+User=sk
+Group=sk
+Type=forking
+Restart=on-failure
+RestartSec=5
+StartLimitInterval=60s
+StartLimitBurst=3
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee -a > $HOME/tmp/test.txt
+sudo systemctl daemon-reload
+sudo systemctl enable dropbox
+sudo systemctl start dropbox
+#__CO__
+
+
 cd $INITIALDIR
 exit 0
 
