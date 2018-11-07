c7a873f4e541a46951acbbc470c7d9d302d00960
tilmitt11191 <tilmitt11@gmail.com>
2018-11-07 16:12:50 +0900

    PC 2018年 11月  7日 水曜日 16:12:50 JST

--- lib/create_symbolic_link.sh
+++ lib/create_symbolic_link.sh
@@ -6,6 +6,16 @@ cd `dirname $0`
 
 
 DATETIME=`date +%Y%m%d%H%M`
+unameOut="$(uname -s)"
+IS_CYGWIN=""
+IS_LINUX=""
+case "${unameOut}" in
+	CYGWIN*) echo "##this is Cygwin" && IS_CYGWIN=true;;
+	Linux*) echo "##this is Linux" && IS_LINUX=true;;
+	*) echo "##this is neither Cygwin nor Linux. exit 1" && exit 1;;
+esac
+
+: <<'#__CO__'
 case "$1" in
 	yes) echo "##mode delete previous dotfiles";;
 	backup) echo "##mode backup previous dotfiles as .***.$DATETIME";;
@@ -24,18 +34,32 @@ else
 fi
 
 DOTFILES=(zshrc vimrc vim tmux.conf tmux zpreztorc zlogout)
-for file in ${DOTFILES[@]}; do
+#for file in ${DOTFILES[@]}; do
 	[ $1 == "backup" ] && [ -e $HOME/.$file ] && cp -r $HOME/.$file $HOME/."$file".$DATETIME
 	[ $1 == "no" ] && [ -e $HOME/.$file ] && cp -r $HOME/.$file $HOME/."$file".$DATETIME
 	[ -e $HOME/.$file ] && rm -rf $HOME/.$file
 	ln -s $HOME/.dotfiles/$file $HOME/.$file
 done
+#__CO__
 
+pwd
 [ -e $HOME/bin/pushgitfiles ] && rm -rf $HOME/bin/pushgitfiles
-ln -s `pwd`/pushgitfiles $HOME/bin/
+ln -s `pwd`/../bin/pushgitfiles $HOME/bin/
 sudo chmod +x $HOME/bin/pushgitfiles
+
+if [ ! $IS_CYGWIN ]; then
+	[ -e $HOME/bin/st ] && rm -rf $HOME/bin/st
+	ln -s `pwd`/../bin/st $HOME/bin/
+	sudo chmod +x $HOME/bin/st
+fi
 export PATH=$HOME/bin:$PATH
 
 
 cd $INITIALDIR
 exit 0
+
+: <<'#__CO__'
+if [ $ ]; then
+	echo "##"
+fi
+#__CO__
