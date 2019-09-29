8a044fd99ecc71f42b60aa469c9e2d8ff4029465
User Takashiozu <takashiozu@www2271.sakura.ne.jp>
2019-09-29 15:31:41 +0900

    Sun Sep 29 15:31:40 JST 2019

--- lib/install_python_manually.sh
+++ lib/install_python_manually.sh
@@ -4,19 +4,20 @@ echo "####`basename $0` start."
 INITIALDIR=`pwd`
 cd `dirname $0`
 
-INSTALL_PYTHON_VERSION=3.6.8
-INSTALL_DIR=$HOME/lib
+INSTALL_PYTHON_VERSION=3.6.9
+INSTALL_DIR="${HOME}/local"
 
-get https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tar.xz --no-check-certificate -O $INSTALL_DIR/python3.6.8
+wget "https://www.python.org/ftp/python/${INSTALL_PYTHON_VERSION}/Python-${INSTALL_PYTHON_VERSION}.tar.xz" --no-check-certificate -O "$INSTALL_DIR/python${INSTALL_PYTHON_VERSION}.tar.xz"
 cd $INSTALL_DIR
-tar xjf Python-"$INSTALL_PYTHON_VERSION".tar.xz
-cd Python-"$INSTALL_PYTHON_VERSION"
+tar xjf "python${INSTALL_PYTHON_VERSION}.tar.xz"
+cd Python-"${INSTALL_PYTHON_VERSION}"
+./configure  --prefix="${INSTALL_DIR}/python-${INSTALL_PYTHON_VERSION}" CPPFLAGS="-I/usr/local/ssl/include" LDFLAGS="-L/usr/local/ssl/lib"
 make
 make install
 
 echo "set PATH as \
 export PATH=\"$INSTALL_DIR/python/bin:$PATH\"\
-export PYTHONPATH=\"$INSTALL_DIR/python/lib/python3.6/site-packages\"\
+export PYTHONPATH=\"$INSTALL_DIR/python/lib/python3.7/site-packages\"\
 export LD_LIBRARY_PATH=\"$INSTALL_DIR/python/lib\"\
 "
 
